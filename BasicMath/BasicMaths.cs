00a25e1af5df7868a424165ccdd967d14907b758
Trusca Alex <alekwtzz@gmail.com>
2019-04-12 01:17:17 +0300

    Added all

---
+++ BasicMath/BasicMaths.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicMath
+{
+    public class BasicMaths
+    {
+      
+       public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Substract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            // To trace error while testing,  writing + operator instead of * operator.
+            return num1 * num2;
+        }
+
+        static void Main(string[] args)
+        {
+            BasicMaths b = new BasicMaths();
+            
+            Console.WriteLine("Adding 2 to 5 = {0}", b.Add(2, 5));
+            Console.WriteLine("Substract 3 from 8 = {0}", b.Substract(8,3));
+            Console.WriteLine("Multiply 4 with 5 = {0}", b.Multiply(4,5));
+            Console.WriteLine("Divide 12 with 3 = {0}", b.Divide(12, 3));
+        }
+    }
+}
---
+++ BasicMathTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicMath;
+
+namespace BasicMathTest
+{
+ 
+    [TestClass]
+    public class UnitTest1
+    {
+ 
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Add(15, 15);
+            Assert.AreEqual(res, 30);
+        }
+
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Substract(30, 5);
+            Assert.AreEqual(res, 25);
+        }
+
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Divide(12, 4);
+            Assert.AreEqual(res, 3);
+        }
+
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Multiply(30, 2);
+            Assert.AreEqual(res, 200);
+        }
+    }
+}
